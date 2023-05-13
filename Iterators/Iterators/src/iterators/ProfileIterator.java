055fe4e630c424f30acd24c7514fa06b29677f33
sharath prakash <sharath.p@swayaan.com>
2023-05-13 13:59:12 +0530

    Iterators

---
+++ Iterators/Iterators/src/iterators/ProfileIterator.java
@@ -0,0 +1,13 @@
+package iterators;
+
+import Profile.Profile;
+
+public interface ProfileIterator {
+
+	  boolean hasNext();
+
+	    Profile getNext();
+
+	    void reset();
+	
+}
---
+++ Iterators/Iterators/src/social_networks/SocialNetwork.java
@@ -0,0 +1,11 @@
+package social_networks;
+
+import iterators.ProfileIterator;
+
+public interface SocialNetwork {
+
+	  ProfileIterator createFriendsIterator(String profileEmail);
+
+	    ProfileIterator createCoworkersIterator(String profileEmail);
+	
+}
