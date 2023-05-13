444b3446d4ae7f89ee617f093ee150aa126caa4c
sharath prakash <sharath.p@swayaan.com>
2023-05-13 16:57:36 +0530

    visitors

---
+++ Visitor/Visitor/src/shapes/Shape.java
@@ -0,0 +1,12 @@
+package shapes;
+
+import visitor.Visitor;
+
+public interface Shape {
+
+
+    void move(int x, int y);
+    void draw();
+    String accept(Visitor visitor);
+	
+}
---
+++ Visitor/Visitor/src/visitor/Visitor.java
@@ -0,0 +1,14 @@
+package visitor;
+import shapes.*;
+
+public interface Visitor {
+	
+	  String visitDot(Dot dot);
+
+	    String visitCircle(Circle circle);
+
+	    String visitRectangle(Rectangle rectangle);
+
+	    String visitCompoundGraphic(CompoundShape cg);
+
+}
